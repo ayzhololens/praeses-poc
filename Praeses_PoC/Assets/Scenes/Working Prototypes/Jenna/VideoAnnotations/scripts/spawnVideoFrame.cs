7c74c5087f5776091bece99ef1ea11328696f350
Jennavee <jvelez@ayzenberg.com>
2016-12-21 18:25:57 -0800

    video test and htoolkit

---
+++ Praeses_PoC/Assets/Scenes/Working Prototypes/Jenna/VideoAnnotations/scripts/spawnVideoFrame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnVideoFrame : MonoBehaviour {
+
+    public GameObject videoPlayer;
+    public GameObject videoFrame;
+    Vector3 spawnPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void spawnMediaPlayer()
+    {
+        spawnPos = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.x + 1);
+        GameObject activeVideoPlayer = Instantiate(videoPlayer, transform.position, Quaternion.identity) as GameObject;
+        GameObject activeVideoFrame = Instantiate(videoFrame, spawnPos, Quaternion.identity) as GameObject;
+    }
+}
---
+++ Praeses_PoC/Assets/Scenes/Working Prototypes/Jenna/VideoAnnotations/scripts/videoRecorder.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.VR.WSA.WebCam;
+using System.Linq;
+#if !UNITY_EDITOR
+    using Windows.Storage;
+    using Windows.System;
+    using System.Collections.Generic;
+using System;
+using System.IO;
+#endif
+
+public class videoRecorder : MonoBehaviour {
+
+
+    VideoCapture m_VideoCapture = null;
+
+	// Use this for initialization
+	void Start () {
+        VideoCapture.CreateAsync(false, OnVideoCaptureCreated);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void startRecordingVideo()
+    {
+        
+    }
+
+
+
+    void OnVideoCaptureCreated(VideoCapture videoCapture)
+    {
+        if(videoCapture != null)
+        {
+            m_VideoCapture = videoCapture;
+
+            Resolution cameraResolution = VideoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).Last();
+            float cameraFramerate = VideoCapture.GetSupportedFrameRatesForResolution(cameraResolution).OrderByDescending((fps) => fps).First();
+
+            CameraParameters cameraParameters = new CameraParameters();
+            cameraParameters.hologramOpacity = 0.0f;
+            cameraParameters.frameRate = cameraFramerate;
+            cameraParameters.cameraResolutionWidth = cameraResolution.width;
+            cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+            m_VideoCapture.StartVideoModeAsync(cameraParameters,
+                                                VideoCapture.AudioState.None,
+                                                OnStartedVideoCaptureMode);
+        }
+        else
+        {
+            Debug.LogError("Failed to create VideoCapture Instance!");
+        }
+    
+    }
+
+    void OnStartedVideoCaptureMode(VideoCapture.VideoCaptureResult result)
+    {
+        if (result.success)
+        {
+            string filename = string.Format("videoTest.mp4", Time.time);
+            string filepath = System.IO.Path.Combine(Application.persistentDataPath, filename);
+
+            m_VideoCapture.StartRecordingAsync(filepath, OnStartedRecordingVideo);
+        }
+    }
+
+    void OnStartedRecordingVideo(VideoCapture.VideoCaptureResult result)
+    {
+
+
+        Debug.Log("Started Recording Video!");
+        // We will stop the video from recording via other input such as a timer or a tap, etc.
+    }
+
+    public void StopRecordingVideo()
+    {
+        m_VideoCapture.StopRecordingAsync(OnStoppedRecordingVideo);
+    }
+
+    void OnStoppedRecordingVideo(VideoCapture.VideoCaptureResult result)
+
+    {
+
+        Debug.Log("Stopped Recording Video!");
+        m_VideoCapture.StopVideoModeAsync(OnStoppedVideoCaptureMode);
+    }
+
+    void OnStoppedVideoCaptureMode(VideoCapture.VideoCaptureResult result)
+    {
+        m_VideoCapture.Dispose();
+        m_VideoCapture = null;
+    }
+}
