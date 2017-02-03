9ecfb501601fbdfd243652739ca9028c5d223206
Jennavee <jvelez@ayzenberg.com>
2017-02-03 11:47:30 -0800

    cleaned radial code

--- Praeses_PoC/Assets/Asset Depot/Scripts/UI/radialManagement.cs
+++ Praeses_PoC/Assets/Asset Depot/Scripts/UI/radialManagement.cs
@@ -41,106 +41,51 @@ namespace HoloToolkit.Unity
             canOpen = true;
         }
 
-        // Update is called once per frame
-        void Update()
+
+        void commentedCode()
         {
+            /* UPDATE
+                        if (radialMode == 0)
+                        {
 
-            if (radialMode == 0)
-            {
+                            RadialMenu.transform.LookAt(Camera.main.transform);
+                            if (gestManager.sourcePressed && !isActive && !annotManager.annotating && gazeManager.FocusedObject.tag != "Node")
+                            {
+                                turnOnRadialMenu();
+                            }
 
-                RadialMenu.transform.LookAt(Camera.main.transform);
-                if (gestManager.sourcePressed && !isActive && !annotManager.annotating && gazeManager.FocusedObject.tag != "Node")
-                {
-                    turnOnRadialMenu();
-                }
+                            if (!gestManager.sourcePressed && isActive && !annotManager.annotating)
+                            {
+                                turnOffRadialMenu();
+                            }
 
-                if (!gestManager.sourcePressed && isActive && !annotManager.annotating)
-                {
-                    turnOffRadialMenu();
-                }
+                            if (gazeManager.FocusedObject != null && gazeManager.FocusedObject.tag == "Button")
+                            {
+                                focusedButton = gazeManager.FocusedObject;
+                            }
+                            else if (gazeManager.FocusedObject == null)
+                            {
+                                focusedButton = null;
+                            }
+                        }
 
-                if (gazeManager.FocusedObject != null && gazeManager.FocusedObject.tag == "Button")
-                {
-                    focusedButton = gazeManager.FocusedObject;
-                }
-                else if (gazeManager.FocusedObject == null)
-                {
-                    focusedButton = null;
-                }
-            }
+                        if (radialMode == 1)
+                        {
+                            RadialMenu.transform.LookAt(Camera.main.transform);
+                            if (gestManager.sourcePressed && !isActive && !annotManager.annotating && gazeManager.FocusedObject.tag != "Node")
+                            {
 
-            if (radialMode == 1)
-            {
-                RadialMenu.transform.LookAt(Camera.main.transform);
-                if (gestManager.sourcePressed && !isActive && !annotManager.annotating && gazeManager.FocusedObject.tag != "Node")
-                {
+                                turnOnRadialMenu();
 
-                    turnOnRadialMenu();
-
-                }
-
-
-
-            }
-
-            if (radialMode == 2 && canOpen)
-            {
-
-                RadialMenu.transform.LookAt(Camera.main.transform);
-                if (gestManager.sourcePressed && !isActive && !annotManager.annotating && gazeManager.FocusedObject.tag != "Node")
-                {
-                    turnOnRadialMenu();
-                }
+                            }
 
 
 
-
-                if (!gestManager.sourcePressed && isActive && !annotManager.annotating && gazeManager.FocusedObject.tag != "Button")
-                {
-                    radialOpenNotClicked = true;
-
-                }
-
-                if (gestManager.sourcePressed && isActive && radialOpenNotClicked  && gazeManager.FocusedObject.tag != "Button")
-                {
-                    turnOffRadialMenu();
-                    radialOpenNotClicked = false;
-                }
-
-                if (!gestManager.sourcePressed && isActive && !annotManager.annotating && gazeManager.FocusedObject.tag == "Button" && !radialOpenNotClicked)
-                {
-                    turnOffRadialMenu();
-                }
-
-                if (gazeManager.FocusedObject != null && gazeManager.FocusedObject.tag == "Button")
-                {
-                    focusedButton = gazeManager.FocusedObject;
-                }
-                else if (gazeManager.FocusedObject == null)
-                {
-                    focusedButton = null;
-                }
-
-                if ((gazeManager.FocusedObject.tag != "Button" && gazeManager.FocusedObject.tag != "Backplate") || radialOpenNotClicked)
-                {
-                    line.GetComponent<LineTest>().line.SetActive(false);
-                    line.SetActive(false);
-                    Debug.Log("line off");
-                }
-                else if (!line.activeSelf && (gazeManager.FocusedObject.tag == "Button" || gazeManager.FocusedObject.tag == "Backplate") && !radialOpenNotClicked)
-                {
-                    line.SetActive(true);
-                    line.GetComponent<LineTest>().line.SetActive(true);
-                    Debug.Log("line on");
-
-                }
-
-                Debug.Log(gazeManager.FocusedObject.tag);
+                        }
+                        */
 
 
-            }
-
-            if (radialMode == 3 && switcher.optionCounter == 4)
+            /*            if (radialMode == 3 && switcher.optionCounter == 4)
             {
                 RadialMenu.transform.LookAt(Camera.main.transform);
                 if (gestManager.sourcePressed && !isActive && gazeManager.FocusedObject.tag != "Node")
@@ -205,6 +150,72 @@ namespace HoloToolkit.Unity
 
                 Debug.Log(gazeManager.FocusedObject.tag);
             }
+            */
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+            if (radialMode == 2 && canOpen)
+            {
+
+                RadialMenu.transform.LookAt(Camera.main.transform);
+
+                if (gazeManager.FocusedObject != null && gazeManager.FocusedObject.tag == "Button")
+                {
+                    focusedButton = gazeManager.FocusedObject;
+                }
+                else if (gazeManager.FocusedObject == null)
+                {
+                    focusedButton = null;
+                }
+
+                if ((gazeManager.FocusedObject.tag != "Button" && gazeManager.FocusedObject.tag != "Backplate") || radialOpenNotClicked)
+                {
+                    line.GetComponent<LineTest>().line.SetActive(false);
+                    line.SetActive(false);
+                    Debug.Log("line off");
+                }
+                else if (!line.activeSelf && (gazeManager.FocusedObject.tag == "Button" || gazeManager.FocusedObject.tag == "Backplate") && !radialOpenNotClicked)
+                {
+                    line.SetActive(true);
+                    line.GetComponent<LineTest>().line.SetActive(true);
+                    Debug.Log("line on");
+
+                }
+
+                if (gestManager.sourcePressed && !isActive && !annotManager.annotating && gazeManager.FocusedObject.tag != "Node")
+                {
+                    turnOnRadialMenu();
+                }
+
+                if (!gestManager.sourcePressed && isActive && !annotManager.annotating && gazeManager.FocusedObject.tag != "Button")
+                {
+                    radialOpenNotClicked = true;
+
+                }
+
+                if (gestManager.sourcePressed && isActive && radialOpenNotClicked  && gazeManager.FocusedObject.tag != "Button")
+                {
+                    turnOffRadialMenu();
+                    radialOpenNotClicked = false;
+                }
+
+                if (!gestManager.sourcePressed && isActive && !annotManager.annotating && gazeManager.FocusedObject.tag == "Button" && !radialOpenNotClicked)
+                {
+                    turnOffRadialMenu();
+                }
+
+
+
+                Debug.Log(gazeManager.FocusedObject.tag);
+
+
+            }
+
+
 
 
         }
@@ -222,17 +233,11 @@ namespace HoloToolkit.Unity
 
             }
 
-            if (radialCounter == 0)
-            {   
-
-            }
-
         }
 
         public void turnOffRadialMenu()
         {
-            canOpen = false;
-            Invoke("opener", .5f);
+            
 
             if(focusedButton == null)
             {
@@ -241,41 +246,34 @@ namespace HoloToolkit.Unity
                 RadialMenu.transform.position = RadialHolder.position;
                 RadialMenu.transform.LookAt(Camera.main.transform);
                 isActive = false;
+                line.SetActive(false);
+                line.GetComponent<LineTest>().line.SetActive(false);
             }
 
             if (focusedButton != null)
             {
-                if(radialMode == 0)
-                {
-                    BroadcastMessage("OnGazeLeave");
-                    focusedButton.SendMessage("OnSelect", SendMessageOptions.DontRequireReceiver);
-                    RadialMenu.SetActive(false);
-                    RadialMenu.transform.position = RadialHolder.position;
-                    RadialMenu.transform.LookAt(Camera.main.transform);
-                    isActive = false;
-                }   
+                //if(radialMode == 0)
+                //{
+                //    BroadcastMessage("OnGazeLeave");
+                //    focusedButton.SendMessage("OnSelect", SendMessageOptions.DontRequireReceiver);
+                //    RadialMenu.SetActive(false);
+                //    RadialMenu.transform.position = RadialHolder.position;
+                //    RadialMenu.transform.LookAt(Camera.main.transform);
+                //    isActive = false;
+                //}   
 
-                if(radialMode == 1)
-                {
-                    BroadcastMessage("OnGazeLeave");
-                    RadialMenu.SetActive(false);
-                    RadialMenu.transform.position = RadialHolder.position;
-                    RadialMenu.transform.LookAt(Camera.main.transform);
-                    isActive = false;
-                }
+                //if(radialMode == 1)
+                //{
+                //    BroadcastMessage("OnGazeLeave");
+                //    RadialMenu.SetActive(false);
+                //    RadialMenu.transform.position = RadialHolder.position;
+                //    RadialMenu.transform.LookAt(Camera.main.transform);
+                //    isActive = false;
+                //}
 
-                if(radialMode == 2 || radialMode == 3)
+                if(radialMode == 2)
                 {
-                    if (up != null)
-                    {
-                        up.SetActive(false);
-                    }
-                    if(down != null)
-                    {
-                        down.SetActive(false);
-                    }
-                    
-                    
+                                 
                     BroadcastMessage("OnGazeLeave");
                     focusedButton.SendMessage("OnSelect", SendMessageOptions.DontRequireReceiver);
                     RadialMenu.SetActive(false);
@@ -285,6 +283,10 @@ namespace HoloToolkit.Unity
                     line.SetActive(false);
                     line.GetComponent<LineTest>().line.SetActive(false);
                 }
+                Invoke("opener", .3f);
+                canOpen = false;
+
+
 
             }
 
@@ -314,45 +316,45 @@ namespace HoloToolkit.Unity
 
         public void switchRadial()
         {
-            turnOffRadialMenu();
-            radialCounter += 1;
+            //turnOffRadialMenu();
+            //radialCounter += 1;
 
-            if (radialCounter >= 3)
-            {
-                radialCounter = 0;
-            }
+            //if (radialCounter >= 3)
+            //{
+            //    radialCounter = 0;
+            //}
 
-            if (radialCounter == 0)
-            {
-                RadialMenu = RadialAlt;
-            }
-            else if (radialCounter == 1)
-            {
-                RadialMenu = RadialAlt2;
-            }
-            else if (radialCounter == 2)
-            {
-                RadialMenu = RadialAlt3;
-            }
+            //if (radialCounter == 0)
+            //{
+            //    RadialMenu = RadialAlt;
+            //}
+            //else if (radialCounter == 1)
+            //{
+            //    RadialMenu = RadialAlt2;
+            //}
+            //else if (radialCounter == 2)
+            //{
+            //    RadialMenu = RadialAlt3;
+            //}
         }
 
         public void Mode()
         {
-            turnOffRadialMenu();
-            radialMode += 1;
-            if(radialMode >= 3)
-            {
-                radialMode = 0;
-            }
+            //turnOffRadialMenu();
+            //radialMode += 1;
+            //if(radialMode >= 3)
+            //{
+            //    radialMode = 0;
+            //}
 
-             if (radialMode == 1)
-            {
-                //RadialMenu.GetComponent<SimpleTagalong>().enabled = true;
-            }
-            else if (radialMode == 0)
-            {
-                //RadialMenu.GetComponent<SimpleTagalong>().enabled = false;
-            }
+            // if (radialMode == 1)
+            //{
+            //    //RadialMenu.GetComponent<SimpleTagalong>().enabled = true;
+            //}
+            //else if (radialMode == 0)
+            //{
+            //    //RadialMenu.GetComponent<SimpleTagalong>().enabled = false;
+            //}
         }
     }
 }
