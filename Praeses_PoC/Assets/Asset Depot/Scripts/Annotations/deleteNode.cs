de5b10f377523919c85ca52cca250071a2fe453b
Jennavee <jvelez@ayzenberg.com>
2017-03-09 10:51:20 -0800

    node delete

---
+++ Praeses_PoC/Assets/Asset Depot/Scripts/Annotations/deleteNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using HoloToolkit.Unity;
+
+public class deleteNode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DeleteNode()
+    {
+        foreach (GameObject node in annotationManager.Instance.activeAnnotations)
+        {
+            if(node == this.gameObject)
+            {
+                if (GetComponent<openAnnotationNode>().miniNode != null)
+                {
+                    DestroyImmediate(GetComponent<openAnnotationNode>().miniNode);
+                }
+                
+                if (GetComponent<annotationMediaHolder>().filepath != null)
+                {
+                    if (File.Exists(GetComponent<annotationMediaHolder>().filepath))
+                    {
+                        File.Delete(GetComponent<annotationMediaHolder>().filepath);
+                    }
+                }
+                DestroyImmediate(this.gameObject);
+                Debug.Log("found it");
+            }
+        }
+    }
+}
