96b54fd783fb0b19a87bf272e3c45a9f338e8a03
cupit13 <jeff.johar@gmail.com>
2017-03-15 12:25:49 -0700

    xray objects behind

--- Praeses_PoC/Assets/Asset Depot/Art/Shaders/occlusion/occludedObject.cs
+++ Praeses_PoC/Assets/Asset Depot/Art/Shaders/occlusion/occludedObject.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class occludedObject : MonoBehaviour {
 
+    public int render = 2000;
+
 	// Use this for initialization
 	void Start () {
         Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer>();
         foreach(Renderer renderer in renderers)
         {
-            renderer.material.renderQueue = 2002;
-        } 
+            //renderer.material.renderQueue = 2002;
+            renderer.material.renderQueue = render;
+        }
+        
 	}
-	
+
 }
---
+++ Praeses_PoC/Assets/Asset Depot/Scripts/Annotations/xrayRaycast.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class xrayRaycast : MonoBehaviour {
+
+    float distance = 50.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+        if (gameObject.GetComponent<Collider>().Raycast(ray, out hit, distance))
+        {
+            //Debug.Log(hit.transform.name);
+            //blockingObject.layer = "ignoreRaycast";
+            GazeManager.Instance.HitObject = gameObject;
+            //print(GazeManager.Instance.HitObject);
+        }
+        //Debug.DrawLine(ray.origin, ray.origin + ray.direction * distance);
+}
+
+    public void test()
+    {
+        print("eggg");
+    }
+}
