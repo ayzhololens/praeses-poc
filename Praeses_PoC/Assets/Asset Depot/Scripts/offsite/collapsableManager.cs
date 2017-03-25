33a1dd4b7c1ff60e98bd5d32faaba1abae8ac9a3
cupit13 <jeff.johar@gmail.com>
2017-03-25 11:46:30 -0700

    moved stuff from working prototype to commit to annj

---
+++ Praeses_PoC/Assets/Asset Depot/Scripts/offsite/collapsableManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collapsableManager : MonoBehaviour {
+
+    public float startCollapse = 1400;
+    public GameObject content;
+
+
+    void adjustToBox(collapseableBox box)
+    {
+        if (box.boxState)
+        {
+            startCollapse += box.expandSize;
+        }
+        else
+        {
+            startCollapse -= box.expandSize;
+        }
+    }
+
+    public void readjustBox()
+    {
+        content.GetComponent<RectTransform>().sizeDelta = new Vector2(content.GetComponent<RectTransform>().rect.width,
+            startCollapse);
+    }
+}
